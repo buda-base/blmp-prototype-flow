a04c75015119e05dd3a706e663a6c3e658704140
Tim Lewis <tim@phurba.net>
2017-06-17 00:48:40 +1200

    Add getter/setters.

--- src/lib/RDFProperty.js
+++ src/lib/RDFProperty.js
@@ -3,8 +3,8 @@
 export default class RDFProperty {
     _IRI: string;
     _superProperties: RDFProperty[];
-    _range = {};
-    _domains = [];
+    _ranges: string[] = [];
+    _domains: string[] = [];
 
     constructor(IRI: string) {
         this._IRI = IRI;
@@ -23,5 +23,29 @@ export default class RDFProperty {
             this._superProperties = [];
         }
         this._superProperties.push(property);
+    }
+
+    get ranges(): string[] {
+        return this._ranges;
+    }
+
+    hasRange(range: string): boolean {
+        return this._ranges.indexOf(range) !== -1;
+    }
+
+    addRange(range: string) {
+        this._ranges.push(range);
+    }
+
+    get domains(): string[] {
+        return this._domains;
+    }
+
+    hasDomain(domain: string): boolean {
+        return this._domains.indexOf(domain) !== -1;
+    }
+
+    addDomain(domain: string) {
+        this._domains.push(domain);
     }
 }
