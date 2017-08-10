2144901f0cf80a5c2440d2a31e2686913bb77f46
Tim Lewis <tim@phurba.net>
2017-08-10 19:48:40 +1200

    Support the new IRI's.

--- src/lib/formatIRI.js
+++ src/lib/formatIRI.js
@@ -2,32 +2,25 @@
 import capitalize from './capitalize'
 
 const BDRC_ONTOLOGY = 'http://purl.bdrc.io/ontology/';
+const RESOURCE = 'http://purl.bdrc.io/resource/';
 const RDF = 'http://www.w3.org/1999/02/22-rdf-syntax-ns#';
 const RDFS = 'http://www.w3.org/2000/01/rdf-schema#';
 
 export default function formatIRI(IRI: string): string {
     let value = (IRI) ? IRI : "";
-
-    if (IRI.indexOf(BDRC_ONTOLOGY) !== -1) {
-        let endPath = IRI.replace(BDRC_ONTOLOGY, '');
-        let segments = endPath.split('#');
-        if (segments.length > 1) {
-            switch(segments[0]) {
-                case 'place':
-                case 'root':
-                default:
-                    value = segments[1];
-
-            }
-        }
-    } else {
-        for (let ns of [RDF, RDFS]) {
-            if (IRI.indexOf(ns) !== -1) {
-                value = IRI.replace(ns, '');
-                break;
-            }
+    let prefixes = [BDRC_ONTOLOGY, RESOURCE, RDF, RDFS];
+    for (let prefix of prefixes) {
+        if (IRI.indexOf(prefix) !== -1) {
+            value = IRI.replace(prefix, '');
+            break;
         }
     }
 
-    return capitalize(value);
+    if (value.indexOf('/') !== -1) {
+        return value.replace(/^(.+)\/(.)(.*)/gm, (match, parentPath, first, remaining) => {
+            return parentPath + '/' + first.toUpperCase() + remaining;
+        });
+    } else {
+        return capitalize(value);
+    }
 }
