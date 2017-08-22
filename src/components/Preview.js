a39cb62be5b8a49433cd38db21432c5c9d99d976
Tim Lewis <tim@phurba.net>
2017-08-22 23:43:32 +1200

    Add syntax highlighting to the turtle view.

--- src/components/Preview.js
+++ src/components/Preview.js
@@ -1,18 +1,33 @@
 import React, { Component } from 'react';
 import './Preview.css';
+import hljs from 'highlight.js/lib/highlight';
+import ttl from '../highlight_languages/ttl';
+import 'highlight.js/styles/github-gist.css';
+
+hljs.registerLanguage('ttl', ttl);
 
 export default class Preview extends Component {
+    _code: Component<any>;
+
     constructor(props) {
         super(props);
+    }
+
+    componentDidUpdate() {
+        hljs.highlightBlock(this._code);
     }
 
     render() {
         return (
             <div className="graphPreview">
                 <h3>Turtle Preview</h3>
-                <div>
-                {this.props.graphText}
-                </div>
+                <pre>
+                    <code className="turtle"
+                        ref={(el => this._code = el).bind(this)}
+                    >
+                        {this.props.graphText}
+                    </code>
+                </pre>
             </div>
         )
     }
