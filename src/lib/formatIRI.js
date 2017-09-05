30760c71b387e69f53b45587c94cb7059b3b9201
Tim Lewis <tim@phurba.net>
2017-09-05 23:17:52 +1200

    Update IRI formatting to take into account the updated bdrc.owl.

--- src/lib/formatIRI.js
+++ src/lib/formatIRI.js
@@ -1,14 +1,17 @@
 // @flow
 import capitalize from './capitalize'
 
-const BDRC_ONTOLOGY = 'http://purl.bdrc.io/ontology/';
+const BDRC_ONTOLOGY = 'http://purl.bdrc.io/ontology/core/';
+const ADMIN = 'http://purl.bdrc.io/ontology/admin/';
 const RESOURCE = 'http://purl.bdrc.io/resource/';
 const RDF = 'http://www.w3.org/1999/02/22-rdf-syntax-ns#';
 const RDFS = 'http://www.w3.org/2000/01/rdf-schema#';
+const SKOS = 'http://www.w3.org/2004/02/skos/core#';
+const TBR = 'http://purl.bdrc.io/ontology/toberemoved/';
 
 export default function formatIRI(IRI: string): string {
     let value = (IRI) ? IRI : "";
-    let prefixes = [BDRC_ONTOLOGY, RESOURCE, RDF, RDFS];
+    let prefixes = [BDRC_ONTOLOGY, ADMIN, RESOURCE, RDF, RDFS, SKOS, TBR];
     for (let prefix of prefixes) {
         if (IRI.indexOf(prefix) !== -1) {
             value = IRI.replace(prefix, '');
