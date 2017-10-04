a986e645b9c3636253e63a7b221978699c8ca989
Tim Lewis <tim@phurba.net>
2017-10-04 22:10:27 +1300

    Add ability to get ontology and individuals to API.

--- src/api/api.js
+++ src/api/api.js
@@ -1,4 +1,8 @@
+// @flow
 import md5 from 'md5';
+import Graph from '../lib/Graph';
+import Ontology from '../lib/Ontology';
+import Individual from '../lib/Individual';
 
 const directoryPrefixes = {
     'C': 'corporations',
@@ -12,16 +16,26 @@ const directoryPrefixes = {
     'T': 'topics',
     'W': 'works'
 }
+
 const OBJECT_PATH = '/objects';
+const ONTOLOGY_PATH = '/bdrc.owl'
+const ONTOLOGY_BASE_IRI = 'http://purl.bdrc.io/ontology/core/';
+const BASE_IRI = 'http://purl.bdrc.io/resource/';
+const TURTLE_MIME_TYPE = 'text/turtle';
+
+export interface APIResponse {
+    text(): Promise<string>
+}
 
 type APIOptions = {
     server?: string,
-    fetch?: (req: Request) => Promise<(res: Response) => void>
+    fetch?: (req: Request) => Promise<*>
 }
 
 export default class API {
     _server: string;
-    _fetch: (req: Request) => Promise
+    _fetch: (req: Request) => Promise<APIResponse>
+    _ontology: Ontology;
 
     constructor(options: ?APIOptions) {
         if (options) {
@@ -30,7 +44,7 @@ export default class API {
         }
     }
 
-    getFileContents(url: string): Promise {
+    getURLContents(url: string): Promise<string> {
         const req = new Request(url);
         let text;
         return new Promise((resolve, reject) => {
@@ -42,11 +56,60 @@ export default class API {
             });
         });
     }
+
+    async getOntology(): Promise<Ontology> {
+        if (!this._ontology) {
+            let ontologyData = await this.getURLContents(ONTOLOGY_PATH);
+            this._ontology = await this._processOntologyData(ontologyData);
+        }
+        
+        return this._ontology;
+    }
+
+    async _processOntologyData(ontologyData: string): Promise<Ontology> {
+        const mimeType = 'application/rdf+xml';
+        let ontology = await Ontology.create(
+            ontologyData, ONTOLOGY_BASE_IRI, mimeType
+        );
+
+        return ontology;
+    }
+
+    /**
+     * Return the full IRI for an object.
+     * 
+     * e.g. if given G844, it would return http://purl.bdrc.io/resource/G844
+     * 
+     * If id is already a valid IRI, that will be returned unchanged.
+     */
+    _getResourceIRI(id: string): string {
+        if (id.indexOf("http://") !== -1) {
+            return id;
+        }
+
+        return BASE_IRI + id;
+    }
+
+    /**
+     * Return the resource id of an object.
+     * 
+     * e.g. for http://purl.bdrc.io/resource/G844 it would return G844
+     * 
+     * If id is not an IRI, it will be returned unchanged.
+     */
+    _getResourceId(id: string): string {
+        if (id.indexOf("http://") === -1) {
+            return id;
+        }
+
+        return id.substr(id.lastIndexOf('/') + 1);
+    }
     
-    getObjectURL(name: string): string | null {
+    _getObjectURL(objectId: string): string | null {
+        const id = this._getResourceId(objectId);
         let firstChars = null;
         try {
-            firstChars = name.match(/^([A-Z]{0,2})/)[0];
+            firstChars = id.match(/^([A-Z]{0,2})/)[0];
         } catch(e) {
             return null;
         }
@@ -56,25 +119,50 @@ export default class API {
             return null;
         }
     
-        const checksum = md5(name);
+        const checksum = md5(id);
         const objectDir = checksum.substr(0, 2);
     
-        let url = [OBJECT_PATH, dir, objectDir, name].join('/') + '.ttl';
+        let url = [OBJECT_PATH, dir, objectDir, id].join('/') + '.ttl';
         if (this._server) {
             url = this._server + url;
-            console.log('server url: %s', url);
         }
         return url;
     }
     
-    getObjectData(name: string): Promise<string> | null {
-        const url = this.getObjectURL(name);
+    async _getObjectData(id: string): Promise<string | null> {
+        const url = this._getObjectURL(id);
         if (url) {
-            return this.getFileContents(url);
+            return this.getURLContents(url);
         } else {
             return null;
         }
+    }
+
+    
+    async getObject(id: string): Promise<Individual | null> {
+        let data: string;
+        try {
+            data = String(await this._getObjectData(id));
+        } catch(e) {
+            return null;
+        }
+
+        let ontology;
+        try {
+            ontology = await this.getOntology();
+        } catch(e) {
+            return null;
+        }
         
+        let graph;
+        try {
+            graph = await Graph.create(data, BASE_IRI, TURTLE_MIME_TYPE, ontology);
+        } catch(e) {
+            return null;
+        }
+    
+        const ind = graph.getIndividualWithId(this._getResourceIRI(id));
+        return ind;
     }
 }
 
--- src/api/api.test.js
+++ src/api/api.test.js
@@ -1,44 +1,119 @@
 // @flow
-import api from './api';
+import api, { APIResponse } from './api';
+import Ontology from '../lib/Ontology';
+import Individual from '../lib/Individual';
 
-const object = 'G843';
+const OBJECT = 'G844';
+const OBJECT_PATH = '/objects/places/ea/G844.ttl';
 
-const SUCCESS = 'success';
+const ONTOLOGY_SUCCESS = `
+<?xml version="1.0"?>
+<rdf:RDF xmlns="http://purl.bdrc.io/ontology/core/"
+     xml:base="http://purl.bdrc.io/ontology/core/"
+     xmlns:owl="http://www.w3.org/2002/07/owl#"
+     xmlns:rdfs="http://www.w3.org/2000/01/rdf-schema#"
+     xmlns:rdf="http://www.w3.org/1999/02/22-rdf-syntax-ns#"
+     xmlns:xml="http://www.w3.org/XML/1998/namespace"
+     >
+    <owl:Ontology rdf:about="http://purl.bdrc.io/ontology/core/">
+        <rdfs:label rdf:datatype="http://www.w3.org/2001/XMLSchema#string">BDRC Ontology</rdfs:label>
+        <owl:versionInfo rdf:datatype="http://www.w3.org/2001/XMLSchema#string">0.1</owl:versionInfo>
+    </owl:Ontology>
 
-class ResponseMock {
+    <owl:AnnotationProperty rdf:about="http://purl.bdrc.io/ontology/admin/logEntry">
+        <rdfs:comment xml:lang="en">Log entry for updates to BDRC resources</rdfs:comment>
+        <rdfs:label xml:lang="en">log entry</rdfs:label>
+        <rdfs:range rdf:resource="http://purl.bdrc.io/ontology/admin/LogEntry"/>
+        <rdfs:domain rdf:resource="http://purl.bdrc.io/ontology/core/Entity"/>
+    </owl:AnnotationProperty>
+</rdf:RDF>
+`;
+
+const OBJECT_SUCCESS = `@prefix :      <http://purl.bdrc.io/ontology/core/> .
+@prefix bdr:   <http://purl.bdrc.io/resource/> .
+@prefix skos:  <http://www.w3.org/2004/02/skos/core#> .
+@prefix tbr:   <http://purl.bdrc.io/ontology/toberemoved/> .
+
+bdr:G844  a                       :Place ;
+    skos:altLabel                 "འདི་ཕྱག་བཏབ་པའི་དུས་ཚོད་ལ་ལས་༡༡༥༠ལོ་བྲིས་པ་མངོན་མོད། དེ་དུས་ཕྱག་འདེབས་པ།"@bo , "bya pa khri dpon gyi mnga' khongs"@bo-x-ewts ;
+    tbr:place_town_syl            "Cai Gong Tang Xiang" .
+`;
+
+const responseTypes = {
+    ONTOLOGY: "ONTOLOGY",
+    OBJECT: "OBJECT"
+}
+
+type ResponseType = $Keys<typeof responseTypes>
+
+class ResponseMock implements APIResponse {
+    _type: ResponseType;
+
+    constructor(type: ResponseType) {
+        this._type = type;
+    }
+
     text() {
         return new Promise((resolve, reject) => {
-            resolve(SUCCESS);
+            if (this._type === responseTypes.ONTOLOGY) {
+                resolve(ONTOLOGY_SUCCESS);
+            } else {
+                resolve(OBJECT_SUCCESS);
+            }
         });
     }
 }
 
-function fetchMock(req: Request): Promise {
+function fetchMock(req: Request): Promise<APIResponse> {
     return new Promise((resolve, reject) => {
-        const res = new ResponseMock();
+        let res;
+        if (req.url === OBJECT_PATH) {
+            res = new ResponseMock(responseTypes.OBJECT)
+        } else {
+            res = new ResponseMock(responseTypes.ONTOLOGY)
+        }
         resolve(res);
     });
-}    
+}
 
-describe('API', () => {
-    test('Getting object URL', () => {
-        const bdrcAPI = new api();
-        const expectedUrl = '/objects/places/70/G843.ttl';
-        expect(
-            bdrcAPI.getObjectURL(object)
-        ).toEqual(expectedUrl);
-    });
-
-    test('Getting object data', done => {
+describe('Ontology', () => {
+    test('Getting ontology', async () => {
         const bdrcAPI = new api({
             fetch: fetchMock
         });
 
-        bdrcAPI.getObjectData(object)
-            .then((text) => {
-                expect(text).toEqual(SUCCESS);
-                done();
-            })
-            .catch();
+        const ontology = await bdrcAPI.getOntology();
+        expect(ontology instanceof Ontology).toBeTruthy();
+
+        const cachedOntology = await bdrcAPI.getOntology();
+        expect(ontology === cachedOntology).toBeTruthy();
+    });
+})
+
+describe('Objects', () => {
+    test('Getting object URL', () => {
+        const bdrcAPI = new api();
+        const expectedUrl = OBJECT_PATH;
+        expect(
+            bdrcAPI._getObjectURL(OBJECT)
+        ).toEqual(expectedUrl);
+    });
+
+    test('Getting object data', async () => {
+        const bdrcAPI = new api({
+            fetch: fetchMock
+        });
+
+        const data = await bdrcAPI._getObjectData(OBJECT);
+        expect(data).toEqual(OBJECT_SUCCESS);
+    });
+
+    test('Get object', async () => {
+        const bdrcAPI = new api({
+            fetch: fetchMock
+        });
+
+        const ind = await bdrcAPI.getObject(OBJECT);
+        expect(ind instanceof Individual).toBeTruthy();
     });
 });
