2913d09086d79c4400290e6b09f75bc14c69f7b8
berger-n <quai.ledrurollin@gmail.com>
2019-01-17 15:30:46 +0100

    added iDB storing .ttl resources

--- src/offline/populateDB.js
+++ src/offline/populateDB.js
@@ -12,24 +12,54 @@ async function populateDB(api) {
      return;
    }
 
-   var dbPromise = await idb.openDb('test-db', 1).then(upgradeDb => {
-      console.log('making a new object store');
-      if (!upgradeDb.objectStoreNames.contains('firstOS')) {
-         upgradeDb.createObjectStore('firstOS');
+   var openRequest = indexedDB.open("blmp-db",1);
+	openRequest.onupgradeneeded = function(e) {
+		console.log("running onupgradeneeded");
+		var thisDb = e.target.result;
+
+		if(!thisDb.objectStoreNames.contains("objects")) {
+			console.log("need to make the objectstore");
+			let objectStore = thisDb.createObjectStore("objects");
+			//objectStore.createIndex("title", "title", { unique: false });
+		}
+	}
+
+	openRequest.onsuccess = async function(e) {
+		var db = e.target.result;
+
+		db.onerror = function(event) {
+		   console.error("Database error: " + event.target.errorCode);
+		};
+
+		console.log("idb",db);
+
+      let i = 0
+      for(let d of personsID) {
+         let res = await api._getResourceData(d.res.value)
+         //console.log("res",res)
+
+         var transaction = db.transaction(["objects"], "readwrite");
+         transaction.oncomplete = function(event) {
+            console.log("Transaction ok");
+         };
+
+         transaction.onerror = function(event) {
+   		   console.error("Transaction error: " + transaction.error);
+         };
+
+         var objects = transaction.objectStore("objects");
+
+         var objRequest = objects.put(res,d.res.value);
+         objRequest.onsuccess = function(event) {
+           console.log("Added",d.res.value);
+         };
+
+         i++;
+         if(i > 1000) break ;
       }
-   });
+	}
 
-   console.log("isok",dbPromise);
 
-   /*   let i = 0
-   for(let d of personsID) {
-      let res = api._getResourceData(d.res.value)
-      console.log("res",res)
-
-      i++;
-      if(i > 100) break ;
-   }
-   */
 
 
 }
